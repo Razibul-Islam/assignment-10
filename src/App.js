e9f0f25a23b68d8d1694d0dab56be4d43325d5ae
Razibul Islam <razibulislam665@gmail.com>
2022-10-26 22:46:01 +0600

    error page done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Error from "./Components/Error/Error";
+import Home from "./Components/Home/Home";
+import Layout from "./Layout/Layout";
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      errorElement:<Error></Error>,
+      element: <Layout></Layout>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+      ],
+    },
+  ]);
+
+  return<div>
+    <RouterProvider router={router}></RouterProvider>
+  </div>;
 }
 
 export default App;
